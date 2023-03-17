f43220df662ed0330b3040cc3b517e8e0e922e44
dosza <oliveira.daniel109@gmail.com>
2023-03-17 02:52:35 -0300

    Retorna lista de clientes

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,12 +1,32 @@
 package com.algaworks.algalog.api.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algalog.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
 	@GetMapping("/clientes")
-	public String listar() {
-		return "Teste";
+	public List<Cliente> listar(){
+		var cliente1 = new Cliente();
+		cliente1.setId(1L);
+		cliente1.setNome("Joao");
+		cliente1.setEmail("joaodascoves@algaworks.com");
+		cliente1.setTelefone("34 9999-1111");
+		
+		var cliente2 = new Cliente();
+		cliente2.setId(2L);
+		cliente2.setNome("Maria");
+		cliente2.setEmail("mariadasilva@algaworks.com");
+		cliente2.setTelefone("11 97777-2222");
+		
+		
+		
+		return Arrays.asList(cliente1,cliente2);
 	}
-	}
+}
+	

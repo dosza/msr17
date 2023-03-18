4b6a10df83c917460299f0e9e49525f087e36a7f
dosza <oliveira.daniel109@gmail.com>
2023-03-18 00:59:14 -0300

    Configura EntityManagerde Cliente

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.algaworks.algalog.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,23 +13,15 @@ import com.algaworks.algalog.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
+	
+	@PersistenceContext
+	private EntityManager manager ;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar(){
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Joao");
-		cliente1.setEmail("joaodascoves@algaworks.com");
-		cliente1.setTelefone("34 9999-1111");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setEmail("mariadasilva@algaworks.com");
-		cliente2.setTelefone("11 97777-2222");
-		
-		
-		
-		return Arrays.asList(cliente1,cliente2);
+		return manager
+				.createQuery("from Cliente",Cliente.class)
+				.getResultList();
 	}
 }
 	

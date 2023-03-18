5736a5dd59dadbbdb7a20c3d1e85387cf58205b8
dosza <oliveira.daniel109@gmail.com>
2023-03-18 20:51:48 -0300

    Implementa Domain Service para Cliente

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
+import com.algaworks.algalog.domain.service.CatalogoClienteService;
 
 import ch.qos.logback.core.joran.conditional.ThenOrElseActionBase;
 import lombok.AllArgsConstructor;
@@ -27,6 +28,7 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/clientes")
 public class ClienteController {
 	private ClienteRepository clienteRepository;
+	private CatalogoClienteService catalogoClienteService;
 	
 	@GetMapping
 	public List<Cliente> listar(){
@@ -44,7 +46,7 @@ public class ClienteController {
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
-		return clienteRepository.save(cliente);
+		return catalogoClienteService.salvar(cliente);
 	}
 	
 	@PutMapping("/{clienteId}")
@@ -54,7 +56,7 @@ public class ClienteController {
 		}
 		
 		cliente.setId(clienteId);
-		cliente = clienteRepository.save(cliente);
+		cliente = catalogoClienteService.salvar(cliente);
 		return ResponseEntity.ok(cliente);
 		
 	}
@@ -64,7 +66,7 @@ public class ClienteController {
 		if ( ! clienteRepository.existsById(clienteId))
 			return ResponseEntity.notFound().build();
 		
-		clienteRepository.deleteById(clienteId);
+		catalogoClienteService.excluir(clienteId); 
 		return ResponseEntity.noContent().build();
 		
 	}

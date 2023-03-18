9c787fe21bf4b93ef21e7c5873bd08433852ca17
dosza <oliveira.daniel109@gmail.com>
2023-03-18 01:56:41 -0300

    Rota para obter cliente by Id

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,10 +1,15 @@
 package com.algaworks.algalog.api.controller;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
+
 
 import lombok.AllArgsConstructor;
 
@@ -17,5 +22,16 @@ public class ClienteController {
 	public List<Cliente> listar(){
 		return clienteRepository.findAll();
 	}
+	
+	@GetMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		if ( cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+ 	}
 }
 		

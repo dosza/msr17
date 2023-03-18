1599892418cb15af7799086422aeb372ef69704a
dosza <oliveira.daniel109@gmail.com>
2023-03-18 20:52:16 -0300

    Corrige identaçao de código

--- algalog-api/src/main/java/com/algaworks/algalog/api/exceptionhandler/ApiExceptionHandler.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/exceptionhandler/ApiExceptionHandler.java
@@ -20,27 +20,26 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @ControllerAdvice
-public class ApiExceptionHandler  extends ResponseEntityExceptionHandler{
-	
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
 	private MessageSource messageSource;
+
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		// TODO Auto-generated method stub
 		List<Problema.Campo> campos = new ArrayList<>();
-		
-		for(ObjectError  error :ex.getBindingResult().getAllErrors()) {
-			String nome =(( FieldError) error).getField();
-			String mensagem =   messageSource.getMessage(error, LocaleContextHolder.getLocale());
-			campos.add(new Problema.Campo(nome,mensagem));
+
+		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+			String nome = ((FieldError) error).getField();
+			String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+			campos.add(new Problema.Campo(nome, mensagem));
 		}
 		Problema problema = new Problema();
 		problema.setStatus(status.value());
 		problema.setDataHora(LocalDateTime.now());
 		problema.setTitulo("Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente!");
 		problema.setCampos(campos);
-		return handleExceptionInternal(ex, problema	,
-				headers, status, request
-				);
+		return handleExceptionInternal(ex, problema, headers, status, request);
 	}
 }

254ccc1c83db7eda70e16e31552cc0e5230c2664
dosza <oliveira.daniel109@gmail.com>
2023-03-19 02:42:18 -0300

    Configura status e data do pedido

--- algalog-api/src/main/java/com/algaworks/algalog/domain/service/SolicitacaoEntregaService.java
+++ algalog-api/src/main/java/com/algaworks/algalog/domain/service/SolicitacaoEntregaService.java
@@ -1,9 +1,12 @@
 package com.algaworks.algalog.domain.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algalog.domain.model.Entrega;
+import com.algaworks.algalog.domain.model.StatusEntrega;
 import com.algaworks.algalog.domain.repository.EntregaRepository;
 
 import lombok.AllArgsConstructor;
@@ -15,6 +18,8 @@ public class SolicitacaoEntregaService {
 	
 	@Transactional
 	public Entrega solicitar( Entrega entrega) {
+		entrega.setStatus(StatusEntrega.PENDENTE);
+		entrega.setDataPedido(LocalDateTime.now());
 		return entregaRepository.save(entrega);
 	}
 	

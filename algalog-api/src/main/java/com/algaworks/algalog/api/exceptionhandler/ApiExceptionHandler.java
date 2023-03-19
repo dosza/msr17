53ad2c4361f4548411d72a6a34c224e17964e097
dosza <oliveira.daniel109@gmail.com>
2023-03-19 18:17:58 -0300

    Retorna 404 em entregas/{entregaId}/ocorrencias quando entregaId não existir

--- algalog-api/src/main/java/com/algaworks/algalog/api/exceptionhandler/ApiExceptionHandler.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/exceptionhandler/ApiExceptionHandler.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.algaworks.algalog.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algalog.domain.exception.NegocioException;
 
 import lombok.AllArgsConstructor;
@@ -55,4 +56,16 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		problema.setTitulo(ex.getMessage());
 		return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
 	}
+	
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<Object> handleEntidadeNaoEncontrada(NegocioException ex, WebRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		Problema problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setDataHora(OffsetDateTime.now());
+		problema.setTitulo(ex.getMessage());
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
+	}
+	
+
 } 
--- algalog-api/src/main/java/com/algaworks/algalog/domain/service/BuscaEntregaService.java
+++ algalog-api/src/main/java/com/algaworks/algalog/domain/service/BuscaEntregaService.java
@@ -2,7 +2,7 @@ package com.algaworks.algalog.domain.service;
 
 import org.springframework.stereotype.Service;
 
-import com.algaworks.algalog.domain.exception.NegocioException;
+import com.algaworks.algalog.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algalog.domain.model.Entrega;
 import com.algaworks.algalog.domain.repository.EntregaRepository;
 
@@ -14,7 +14,7 @@ public class BuscaEntregaService {
 	private EntregaRepository entregaRepository;
 	public Entrega buscar(Long entregaId) {
 		return entregaRepository.findById(entregaId)
-				.orElseThrow( () -> new NegocioException("Entrega não encontrada"));
+				.orElseThrow( () -> new EntidadeNaoEncontradaException("Entrega não encontrada"));
 		
 	}
 }

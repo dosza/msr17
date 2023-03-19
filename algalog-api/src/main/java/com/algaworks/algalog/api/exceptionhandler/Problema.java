78f99ed747b114c2d79a0aeec8dd8fa332fb5720
dosza <oliveira.daniel109@gmail.com>
2023-03-18 21:18:34 -0300

    Adiciona anotação para o Spring ignorar campos nulos

--- algalog-api/src/main/java/com/algaworks/algalog/api/exceptionhandler/Problema.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/exceptionhandler/Problema.java
@@ -3,6 +3,9 @@ package com.algaworks.algalog.api.exceptionhandler;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,7 +13,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-
+@JsonInclude(Include.NON_NULL)
 public class Problema {
 	private Integer status;
 	private LocalDateTime dataHora;
